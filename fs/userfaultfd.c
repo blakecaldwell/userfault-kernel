7a5ac1db0588fd3f352cd06e46c9a880b5adcbe4
Blake Caldwell <blake.caldwell@colorado.edu>
2015-04-05 16:25:38 +0000

    Add direction flag for UFFDIO_REMAP
    The direction flag in the remap struct indicates which vma list to
    search. This is needed for the use case of evicting a page from
    outside the context of the userfaultfd. For example: eviction from
    a userfault vma in userfault_ctx->mm to a vma in current->mm,
    requires that we validated the src page exists in the vma list of
    userfault->mm, where move a page into the userfaultfd context
    requires that the src page must exist in the vma list of current->mm
    and that the destination page exist in the vma list of
    userfault_ctx->mm.

--- fs/userfaultfd.c
+++ fs/userfaultfd.c
@@ -1674,6 +1674,7 @@ static int userfaultfd_remap(struct userfaultfd_ctx *ctx,
 	struct uffdio_remap uffdio_remap;
 	struct uffdio_remap __user *user_uffdio_remap;
 	struct userfaultfd_wake_range range;
+	struct mm_struct *src_mm, *dst_mm;
 
 	user_uffdio_remap = (struct uffdio_remap __user *) arg;
 
@@ -1682,11 +1683,19 @@ static int userfaultfd_remap(struct userfaultfd_ctx *ctx,
 			   /* don't copy "remap" last field */
 			   sizeof(uffdio_remap)-sizeof(__s64)))
 		goto out;
+		if (uffdio_remap.direction == UFFDIO_REMAP_DIRECTION_OUT) {
+			src_mm = ctx->mm;
+			dst_mm = current->mm;
+		}
+		else {
+			src_mm = current->mm;
+			dst_mm = ctx->mm;
+		}
 
-	ret = validate_range(ctx->mm, uffdio_remap.dst, uffdio_remap.len);
+	ret = validate_range(dst_mm, uffdio_remap.dst, uffdio_remap.len);
 	if (ret)
 		goto out;
-	ret = validate_range(current->mm, uffdio_remap.src, uffdio_remap.len);
+	ret = validate_range(src_mm, uffdio_remap.src, uffdio_remap.len);
 	if (ret)
 		goto out;
 	ret = -EINVAL;
@@ -1694,7 +1703,7 @@ static int userfaultfd_remap(struct userfaultfd_ctx *ctx,
 				  UFFDIO_REMAP_MODE_DONTWAKE))
 		goto out;
 
-	ret = remap_pages(ctx->mm, current->mm,
+	ret = remap_pages(dst_mm, src_mm,
 			  uffdio_remap.dst, uffdio_remap.src,
 			  uffdio_remap.len, uffdio_remap.mode);
 	if (unlikely(put_user(ret, &user_uffdio_remap->remap)))
