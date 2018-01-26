cd59c5a6d93bceb12ca1750205d0286f5f11d0d5
Blake Caldwell <blake.caldwell@colorado.edu>
2018-01-26 20:46:50 +0000

    userfaultfd: add direction flag for UFFDIO_REMAP
    This is needed to support the two use cases, of moving a page from
    a userland anonymous vma into the userfaultfd registered region, or
    vice-versa. Moving a page from the userfaultfd region is necessary
    to remove pages, and generate page fault events if the application
    tries accessing the page again.

--- fs/userfaultfd.c
+++ fs/userfaultfd.c
@@ -1756,6 +1756,7 @@ static int userfaultfd_remap(struct userfaultfd_ctx *ctx,
 	struct uffdio_remap uffdio_remap;
 	struct uffdio_remap __user *user_uffdio_remap;
 	struct userfaultfd_wake_range range;
+	struct mm_struct *src_mm, *dst_mm;
 
 	user_uffdio_remap = (struct uffdio_remap __user *) arg;
 
@@ -1764,11 +1765,19 @@ static int userfaultfd_remap(struct userfaultfd_ctx *ctx,
 			   /* don't copy "remap" last field */
 			   sizeof(uffdio_remap)-sizeof(__s64)))
 		goto out;
+		if (uffdio_remap.mode & UFFDIO_REMAP_MODE_DIRECTION_IN) {
+			src_mm = current->mm;
+			dst_mm = ctx->mm;
+		}
+		else {
+			src_mm = ctx->mm;
+			dst_mm = current->mm;
+		}
 
-	ret = validate_range(ctx->mm, uffdio_remap.dst, uffdio_remap.len);
+	ret = validate_range(dst_mm, uffdio_remap.dst, uffdio_remap.len);
 	if (ret)
 		goto out;
-	ret = validate_range(current->mm, uffdio_remap.src, uffdio_remap.len);
+	ret = validate_range(src_mm, uffdio_remap.src, uffdio_remap.len);
 	if (ret)
 		goto out;
 	ret = -EINVAL;
@@ -1776,7 +1785,7 @@ static int userfaultfd_remap(struct userfaultfd_ctx *ctx,
 				  UFFDIO_REMAP_MODE_DONTWAKE))
 		goto out;
 
-	ret = remap_pages(ctx->mm, current->mm,
+	ret = remap_pages(dst_mm, src_mm,
 			  uffdio_remap.dst, uffdio_remap.src,
 			  uffdio_remap.len, uffdio_remap.mode);
 	if (unlikely(put_user(ret, &user_uffdio_remap->remap)))
