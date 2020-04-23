0a9fe0d804fc72760f9fc7270c390ad15c95ab8c
Blake Caldwell <blake_caldwell@brown.edu>
2020-04-23 10:34:37 -0600

    userfaultfd: update to new __pte_alloc interface
    Since 4cf5892495

--- mm/userfaultfd.c
+++ mm/userfaultfd.c
@@ -1077,8 +1077,8 @@ ssize_t remap_pages(struct mm_struct *dst_mm, struct mm_struct *src_mm,
 				err = -ENOENT;
 				break;
 			} else {
-				if (unlikely(__pte_alloc(src_mm, src_pmd,
-							 src_addr))) {
+				if (unlikely(__pte_alloc(src_mm,
+                                                         src_pmd))) {
 					err = -ENOMEM;
 					break;
 				}
@@ -1097,7 +1097,7 @@ ssize_t remap_pages(struct mm_struct *dst_mm, struct mm_struct *src_mm,
 		}
 
 		if (unlikely(pmd_none(dst_pmdval)) &&
-		    unlikely(__pte_alloc(dst_mm, dst_pmd, dst_addr))) {
+		    unlikely(__pte_alloc(dst_mm, dst_pmd))) {
 			err = -ENOMEM;
 			break;
 		}
